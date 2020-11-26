056dac62ff00751859cd4ed25df26f98ee3ccb6a
Farshad Shafii <farshad.shafii@gmail.com>
2020-11-26 17:39:32 +0000

    added colour to the displayed text, green if it is a palindrome, red if it isn't

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ const button = document.getElementById("submit-button");
 function palindrome() {
   button.addEventListener("click", function () {
     display.innerHTML = text.value.split("").reverse().join("");
+    if (display.innerHTML === text.value) {
+      display.style.color = "green";
+    } else {
+      display.style.color = "red";
+    }
   });
 }
 
