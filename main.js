82002f40fdb1e456b191f407f4f23eff87b7420a
Farshad Shafii <farshad.shafii@gmail.com>
2020-11-26 14:28:50 +0000

    created function, when the button is clicked the value in the box will be turned backwards and displayed at the bottom

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 let text = document.getElementById("palindrome-text-holder");
 let display = document.getElementById("display-palindrome");
 const button = document.getElementById("submit-button");
+
+function palindrome() {
+  button.addEventListener("click", function () {
+    display.innerHTML = text.value.split("").reverse().join("");
+  });
+}
+
+palindrome();
